ccd506976c6b835510eb3ad99a26d9450f3bb9eb
Samuel Maura <smaura777@gmail.com>
2013-06-25 16:38:37 -0400

    Activity CRUD update

--- controllers/activity.js
+++ controllers/activity.js
@@ -59,34 +59,38 @@ exports.create = function(req,res){
            (req.body.name !== undefined) &&
            (req.body.type !== undefined) ){
            
-      		new_activity.id = req.body.id;
-      		new_activity.activity = {};
-      		new_activity.activity.name = req.body.name;
-      		new_activity.activity.type = req.body.type;
-      		new_activity.activity.date = new Date();
-      		new_activity.activity.details = {};
+      		new_activity.uid = req.body.id;
+      		new_activity.name = req.body.name;
+      		new_activity.type = req.body.type;
+      		new_activity.date = new Date();
+      		//new_activity.activity.details = {};
       	
-      		if (req.body.sport !== undefined)
-      			new_activity.activity.details.name = req.body.sport;
       		
       		
       		if (req.body.weight !== undefined)
-      			new_activity.activity.details.weight = req.body.weight;
+      			new_activity.weight = req.body.weight;
       			
       			
-      	    if (req.body.rep !== undefined)
-      			new_activity.activity.details.rep = req.body.rep;		
+      	    if (req.body.reps !== undefined)
+      			new_activity.reps = req.body.reps;		
       		
       		if (req.body.sets !== undefined)
-      			new_activity.activity.details.sets = req.body.sets;		
-      		
-      		// running 	
+      			new_activity.sets = req.body.sets;	
+      			
+      	     if (req.body.weight !== undefined)
+      			new_activity.weight = req.body.weight;				
       		
       		if (req.body.distance !== undefined)
-      			new_activity.activity.details.distance = req.body.distance;		
+      			new_activity.distance = req.body.distance;		
       		
       		if (req.body.speed !== undefined)
-      			new_activity.activity.details.speed = req.body.speed;		
+      			new_activity.speed = req.body.speed;	
+      			
+      	    if (req.body.duration !== undefined)
+      			new_activity.duration = req.body.duration;	
+      		
+      		if (req.body.heart_rate !== undefined)
+      			new_activity.heart_rate = req.body.heart_rate;				
       		
       		
       		console.log(require('util').inspect(new_activity) );
@@ -95,10 +99,10 @@ exports.create = function(req,res){
  		        var acc = db.collection('activity');
  		        acc.insert(new_activity,{w:1}, function(err,result){
  		        	if (!err){
- 						console.log("activity Success");
+ 						console.log("activity creation Success");
  					}
  					else {
- 				  		console.log("activity Failed insert ");
+ 				  		console.log("activity creation Failed insert ");
  					}	
  		        });
  			 });    
